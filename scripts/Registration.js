7b74d7faf1e28021180285b785be801f307a8e01
anonymously-coded <muraliganesan9055@gmail.com>
2022-03-21 19:30:57 +0530

    login started

---
+++ scripts/Registration.js
@@ -0,0 +1,10 @@
+function passwordToggle () {
+const togglePassword = document.querySelector('#togglePassword');
+const pwd = document.querySelector('#pwd');
+const type = pwd.getAttribute('type') === 'password' ? 'text' : 'password';
+pwd.setAttribute('type', type);
+togglePassword.className = togglePassword.className=="bi bi-eye-slash"?"bi bi-eye":"bi bi-eye-slash";
+}
+
+
+
